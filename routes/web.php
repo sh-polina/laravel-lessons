3325b089b076532c9ca87d02e4a7d0da240d1737
sh-polina <polina.shlygina@gmail.com>
2024-04-28 21:58:31 +0300

    init commit

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\ArticlesController;
+use App\Http\Controllers\HelloController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello', [HelloController::class, 'sayHello']);
