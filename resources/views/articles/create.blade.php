65b275985a5f141bd254e473829727694843f333
sh-polina <polina.shlygina@gmail.com>
2024-05-20 02:43:40 +0300

    added bootstrap to create form

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -1,23 +1,38 @@
+@extends('layouts.app')
 @if($errors->any())
     @foreach($errors->all() as $error)
         <div style="color: red"> {{ $error }}</div>
     @endforeach
 @endif
 
-<div>
+@section('content')
     <form action="{{route('articles.store')}}" method="post">
-        @csrf
-        <input name="title" type="text">
-        <textarea name="article_body"></textarea>
-        <select name="user">
-            @foreach($users as $user)
-                <option value="{{ $user->id }}">{{ $user->name }}</option>
-            @endforeach
-        </select>
+        <div class="mb-3">
+            @csrf
+            <label class="form-label"> Название статьи: </label>
+            <input name="title" type="text" class="form-control">
+
+        </div>
+        <div class="mb-3">
+            <label class="form-label"> Статья: </label>
+            <textarea name="article_body" class="form-control"></textarea>
+
+        </div>
+        <div class="mb-3">
+            <select name="user" class="form-select">
+                @foreach($users as $user)
+                    <option value="{{ $user->id }}">{{ $user->name }}</option>
+                @endforeach
+            </select>
+        </div>
+
         @foreach($categories as $category)
-            <input type="checkbox" name="category[]" value="{{ $category->id }}"/>
-            <label for="category">{{ $category->name }}</label>
+            <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" name="category[]" value="{{ $category->id }}"/>
+                <label for="category" class="form-check-label">{{ $category->name }}</label>
+            </div>
         @endforeach
-        <button type="submit">Create</button>
+        <button type="submit" class="btn btn-primary">Create</button>
+
     </form>
-</div>
+@endsection
